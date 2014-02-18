7cc9ed62f8863e52c59e247268198ee82594c57e
Leandro Cicco <leandrocicco@gmail.com>
2014-02-18 19:56:03 -0300

    configurando paperclip para producao

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,4 +1,11 @@
-Paperclip::Attachment.default_options.merge!(
+if Rails.env == "production" || Rails.env == "homologacao"
+  Paperclip::Attachment.default_options.merge!(
+    :path => "/storage/dor-neuro/:attachment/:id/:style/:basename.:extension",
+    :url => "/dorneuro/:attachment/:id/:style/:basename.:extension"
+  )
+else
+  Paperclip::Attachment.default_options.merge!(
     :path => ":rails_root/public/system/:attachment/:id/:style/:basename.:extension",
-    :url => "/dorneuro/system/:attachment/:id/:style/:basename.:extension"
-)
+    :url => "/system/:attachment/:id/:style/:basename.:extension"
+  )
+end
